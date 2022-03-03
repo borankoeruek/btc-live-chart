30815127c95d53ee0d30803e45a92cf5307f865e
vendom2k <borankrk@gmail.com>
2022-03-03 16:13:51 +0100

    example

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import React, { Component } from "react";
 import Plot from "react-plotly.js";
-
+import "./css/app.css";
 class App extends Component {
   state = {};
+
   render() {
-    return <></>;
+    return (
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Plot
+          data={[
+            {
+              x: [1, 2, 3],
+              y: [2, 6, 3],
+              type: "scatter",
+              mode: "lines+markers",
+              marker: { color: "red" },
+            },
+            { type: "bar", x: [1, 2, 3], y: [2, 5, 3] },
+          ]}
+          layout={{
+            width: 700,
+            height: 400,
+            autosize: true,
+            title: "BTC Live Chart",
+          }}
+        />
+      </div>
+    );
   }
 }
 
